9589003bdb5e2e07bc16d63af3f4cf305aabc42e
Change Ju <busbtvi@gmail.com>
2025-02-16 16:46:18 +0900

    make strtok work & why

--- crust_of_rust/07_Subtyping and Variance/strtok/src/lib.rs
+++ crust_of_rust/07_Subtyping and Variance/strtok/src/lib.rs
@@ -1,18 +1,15 @@
-// return의 lifetime을 static으로 바꿔도 여전히 에러
-pub fn strtok<'a>(s: &'a mut &'a str, delimiter: char) {
-    // pub fn strtok<'a>(s: &'a mut &'a str, delimiter: char) -> &'static str {
+// lifetime을 2가지로 분리하면, 처음의 테스트도 정상적으로 작동한다
+pub fn strtok<'a, 'b>(s: &'a mut &'b str, delimiter: char) -> &'b str {
     // pub fn strtok<'a>(s: &'a mut &'a str, delimiter: char) -> &'a str {
     if let Some(i) = s.find(delimiter) {
         let prefix = &s[..i];
         let suffix = &s[(i + delimiter.len_utf8())..];
         *s = suffix;
-        // prefix
-        // ""
+        prefix
     } else {
         let prefix = *s;
         *s = "";
-        // prefix
-        // ""
+        prefix
     }
 }
 
@@ -20,51 +17,37 @@ pub fn strtok<'a>(s: &'a mut &'a str, delimiter: char) {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn it_works() {
-    //     let mut x = "hello world";
-    //     {
-    //         let hello = strtok(&mut x, ' ');
-    //         // assert_eq!(hello, "hello");
-    //         /*
-    //            scope를 추가해도 여전히 컴파일이 안됨
-    //            scope내에서 assert_eq를 사용하지 않아도
-    //            fn strtok의 반환 타입을 'static으로 바꿔도 안됨
-    //            fn strtok의 반환을 없애도 안돔
-    //         */
-    //     }
-    //     assert_eq!(x, "world");
-    // }
+    #[test]
+    fn it_works_now() {
+        let mut x = "hello world";
+        /*
+            다음줄이 에러가 안나는 이유는 z의 lifetime이 줄어들었기 때문인데,
+            이게 가능한 이유는 mutable reference는 lifetie에 대해서 covariance이기 때문
+        */
+        let z = &mut x;
+
+        // strtok<'a,'b>(&'a mut &'b      str) -> &'b      str
+        //               &'? mut &'static str  -> &'static str
+        let hello = strtok(&mut x, ' ');
+        assert_eq!(hello, "hello");
+        assert_eq!(x, "world");
+    }
 
     #[test]
     fn it_works() {
-        /*
-           x: &'static str
-           &mut x: &'? mut &'static str
-           strtok의 첫번째 인자: &'a mut &'a str
-
-           함수의 인자와 파라미터가 같아야 하고, 'a는 공통이어야 하기 때문에
-           &'static mut 'static str 이 되어버림
-           그래서 assert_eq에서도 &mut이 살아있다고 인식
-        */
         let mut x = "hello world";
-        // check_is_static(x);
+        check_is_static(x);
+
+        /*
+            x:          &'?      mut &'static str
+            strtok arg: &'a      mut &'a      str
+
+            &'a mut T는 T에 대해서 invariant이기 때문에, 'a는 'static이 되어야 한다
+            그러면 &'static mut에 &'a mut을 전달하는 상황이기 때문에,
+                - `x` does not live long enough
+        */
         strtok(&mut x, ' ');
         // assert_eq!(x, "world");
-        /*
-            assert를 주석 처리 하면 컴파일이 되는 이유
-            "hello world"는 여전히 'static 이지만, 임의로 scope를 제한시킴
-            lifetime이 긴 것을 짧은것에서도 사용 가능하기 때문
-
-            fn main(){
-                let s = String::new();
-                let x: &'static str = "hello";
-                let mut y/*  y: &'a */ = &*s;
-                y = x; // 'static -> 'a
-
-                // static is subtype of any lifetime of 'a
-            }
-        */
 
         fn check_is_static(_: &'static str) {}
     }
