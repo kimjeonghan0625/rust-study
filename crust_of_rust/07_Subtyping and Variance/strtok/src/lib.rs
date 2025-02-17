2e362fe0da46df0a0e8fd32ee871dfe94dec990e
Change Ju <busbtvi@gmail.com>
2025-02-17 20:45:48 +0900

    drop check example

--- crust_of_rust/07_Subtyping and Variance/strtok/src/lib.rs
+++ crust_of_rust/07_Subtyping and Variance/strtok/src/lib.rs
@@ -13,6 +13,43 @@ pub fn strtok<'a, 'b>(s: &'a mut &'b str, delimiter: char) -> &'b str {
     }
 }
 
+struct TouchDrop<T: std::fmt::Debug>(T);
+impl<T: std::fmt::Debug> Drop for TouchDrop<T> {
+    fn drop(&mut self) {
+        println!("{:?}", self.0);
+    }
+}
+fn main() {
+    let x = String::new();
+    /*
+       vec![&x] 일때는 컴파일 에러가 안나고, TouchDrop일때는 컴파일 에러가 발생
+       vector는 inner type이 drop을 구현할때, vector의 drop시 inner type에 대한 drop을 호출
+        - drop을 구현한 TouchDrop은 컴파일 에러 발생
+        - &T는 drop을 구현 X
+    */
+    // let z = vec![&x];
+    let z = vec![TouchDrop(&x)];
+    drop(x);
+    // drop(z);
+}
+
+use std::marker::PhantomData;
+struct Deserializer<T> {
+    // phantomData<T>는 컴파일러에게 Deserializer가 T를 소유하고 있는것으로 알림
+    // 그래서 T를 drop 하는 것으로 간주
+    _t: PhantomData<T>,
+}
+struct Deserializer2<T> {
+    // 이 signiature는 T를 소유하지 않기 때문에, T에 대한 drop이 발생 X
+    // 그럼 Deserializer3과 같지 않냐?
+    // fn() -> T: covariant, fn(T): contravariant
+    _t: PhantomData<fn() -> T>,
+}
+struct Deserializer3<T> {
+    // contravariant이기 때문에 쓰기 더 어려움
+    _t: PhantomData<fn(T)>,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
